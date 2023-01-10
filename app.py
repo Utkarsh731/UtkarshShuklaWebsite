4986046c956630126395ee377a5e2695986ebaa8
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-10 11:14:38 +0530

    poetry page added

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index_page():
     return render_template('index.html')
 
+@app.route("/poetry")
+def poetry_page():
+    return render_template('poetry.html')
+
 if __name__=='__main__':
     app.run(debug=True)
