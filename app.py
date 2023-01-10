af4c5c903305df2fc97ebb4f196bc542d69b9c65
Utkarsh731 <uttkarsh.shuklas@gmail.com>
2023-01-10 11:49:34 +0530

    poetry page added, data cleaned and photography section is remaining ro be transformed

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index_page():
-    return render_template('index.html')
+
 
 @app.route("/poetry")
 def poetry_page():
     return render_template('poetry.html')
+
+
+@app.route("/")
+def index_page():
+    return render_template('index.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
